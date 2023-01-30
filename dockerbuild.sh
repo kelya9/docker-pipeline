9d613ac8d889b59307ea6967aba01c8fbfae031a
kelya9 <94655040+kelya9@users.noreply.github.com>
2023-01-30 18:56:32 -0500

    test build

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-for ((tag=1;tag <20; tag++))
- do
-echo "$tag"
+tag = ${BUILD_NUMBER}
 docker build -t webapp:$tag .
 docker tag webapp:$tag 145682747025.dkr.ecr.us-east-1.amazonaws.com/dockerstore:$tag
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 145682747025.dkr.ecr.us-east-1.amazonaws.com
 docker push 145682747025.dkr.ecr.us-east-1.amazonaws.com/dockerstore:$tag
 
-done
